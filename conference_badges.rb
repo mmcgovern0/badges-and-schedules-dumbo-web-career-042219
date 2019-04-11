feec2a3305be59335b4912fef95229b2c0a20266
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 04:42:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_maker(name_array)
-  
+def batch_badge_maker(name_list)
+  name_list.map {|name| badge_maker(name)}
+end
   
