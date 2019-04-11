f4e2dd8f6ae4f6b5d7a19a67a559d6a878fb8f97
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 05:15:33 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+atendees = ["Pam", "Jim", "Michael", "Dwight", "Angela", "Ryan", "Creed"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(atendees)
+  atendees.map do |name| 
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(atendees)
+  atendees.each_with_index.map do |atendee, index|
+    "Hello, #{atendee}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(atendees)
+  batch_badge_creator(atendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(atendees).each do |assignment|
+    puts assignment
+  end
+end
