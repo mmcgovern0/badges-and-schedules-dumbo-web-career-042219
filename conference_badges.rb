83bfa62ab8460b29e38a9ef8020d023100ab8beb
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 05:06:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(name_list)
   name_list.each_with_index.map do |name, index|
-    "Hello #{name}! You'll bw assigned to room #{index + 1}!"
+    "Hello #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
 
