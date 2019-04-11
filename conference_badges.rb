226cbb5617970da297d8962b14584e205c9b35a0
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 05:15:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(atendees)
 end
 
 def printer(atendees)
-  batch_badge_creator(atendees) do |badge|
+  batch_badge_creator(atendees).each do |badge|
     puts badge
   end
   assign_rooms(atendees).each do |assignment|
